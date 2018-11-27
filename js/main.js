3496f6f10003d0938dee79dcb4f563631b2abf80
blosterr <blosterr@users.noreply.github.com>
2018-11-27 17:57:36 +0200

    0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// $( ".menu_btn" ).click(function() {
+//   $( ".bot_header" ).removeClass( "d-none" ).addClass( "d-block" )
+// });
+
+
+$(".menu_btn").click(function(){
+    $(".bot_header").toggle();
+});
